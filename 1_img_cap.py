c76fe2aa00b1845ce0c314f35304778d2a35f336
mdom0002 <54925718+mdom0002@users.noreply.github.com>
2021-01-13 12:04:45 +0530

    Update 1_img_cap.py

--- 1_img_cap.py
+++ 1_img_cap.py
@@ -12,32 +12,32 @@ exit_con='**'
 
 a=''
 
-dir0=input('enter the directory name : ')
+dir0=input('enter the directory name : ')   #enter a directory name, means a folder name in our case eg: ImageProcessing
 
 try:
-    os.mkdir(dir0)
+    os.mkdir(dir0)       #make directory with the input name
 except:
-    print('contain folder in same name')
+    print('contain folder in same name')   #if a folder already exists then print
 
 # get the reference to the webcam
 camera = cv2.VideoCapture(0)
 
 while(True):
 
-    a=input('exit: ** or enter the label name : ')
+    a=input('exit: ** or enter the label name : ')  #insert the label name or input ** to exit
 
-    if a==exit_con:
+    if a==exit_con:       #if user input is **, break from the loop
         break
 
-    dir1=str(dir0)+'/'+str(a)
-    print(dir1)
+    dir1=str(dir0)+'/'+str(a)    #dir1 = directoryname given before as dir0 / label name given as a -basically makes a path- eg: ImageProcessing/Signs
+    print(dir1)    
 
     try:
-        os.mkdir(dir1)
+        os.mkdir(dir1)    #make directory - basically creates a folder with the label name inserted by user inside the directory input by the user
     except:
         print('contain folder')
 
-    i=0
+    i=0   #i initialized as 0 which would be the name of the first jpg file
 
     while(True):
         (t, frame) = camera.read()
@@ -47,7 +47,7 @@ while(True):
         # flip the frame so that it is not the mirror view
         frame = cv2.flip(frame, 1)
 
-        # get the ROI
+        # get the ROI  - ROI = image[y1:y2, x1:x2]
         roi = frame[top:bottom, right:left]
 
         # convert the roi to grayscale and blur it
@@ -58,8 +58,8 @@ while(True):
         gray = cv2.resize(gray, (IMG_SIZE,IMG_SIZE))
 
         #write img file to directory
-        cv2.imwrite("%s/%s/%d.jpg"%(dir0,a,i),gray)
-        i+=1
+        cv2.imwrite("%s/%s/%d.jpg"%(dir0,a,i),gray)   #write the image to %s/%s/%d.jpg which would be replaced by dir0/a/i.jpg - eg: ImageProcessing/Signs/0.jpg
+        i+=1   #increment value of i  ,ie, i = i + 1
         print(i)
         if i>500:
             break
