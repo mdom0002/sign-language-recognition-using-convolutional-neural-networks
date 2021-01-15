e20a0405986d5c9d31d4cade571563d4a70a7f91
mdom0002 <54925718+mdom0002@users.noreply.github.com>
2021-01-15 12:54:16 +0530

    Update 2_create_dataset.py

--- 2_create_dataset.py
+++ 2_create_dataset.py
@@ -7,26 +7,26 @@ path='data'
 
 IMG_SIZE = 96
 
-def create_train_data():
-    training_data = []
+def create_train_data():      #function header
+    training_data = []        #declare empty array
     label=0
-    for (dirpath,dirnames,filenames) in os.walk(path):
-        for dirname in dirnames:
+    for (dirpath,dirnames,filenames) in os.walk(path):         
+        for dirname in dirnames:                                # for each directory name
             print(dirname)
             for(direcpath,direcnames,files) in os.walk(path+"/"+dirname):
-                for file in files:
-                        actual_path=path+"/"+dirname+"/"+file
+                for file in files:                               #for each file in the directory
+                        actual_path=path+"/"+dirname+"/"+file     #sets actual_path as path/dirctoryname/filename
                         print(files)
                         # label=label_img(dirname)
                         path1 =path+"/"+dirname+'/'+file
-                        img=cv2.imread(path1,cv2.IMREAD_GRAYSCALE)
-                        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-                        training_data.append([np.array(img),label])
-            label=label+1
+                        img=cv2.imread(path1,cv2.IMREAD_GRAYSCALE)  # greyscale of image with the specific path           
+                        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))  #resize to 96 as initialized before
+                        training_data.append([np.array(img),label])  #append the image and label(first case will be 0) to training_data array declared before
+            label=label+1               #increment label value by 1
             print(label)
-    shuffle(training_data)
-    np.save('train_data.npy', training_data)
+    shuffle(training_data)              #shuffle the array
+    np.save('train_data.npy', training_data)   #save array to binary file in numpy format
     print(training_data)
-    return training_data
+    return training_data                      #return thr array with images and labels
 
-create_train_data()
+create_train_data()                             #call the function
